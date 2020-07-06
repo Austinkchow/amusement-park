df04bd236db770b7f5854e9d7ccde1dfa5450eed
j0nathanp3nEnterprise <jonathanpen1230@gmail.com>
2020-07-06 17:40:14 -0400

    adds database

--- amusementPark/amusementPark/settings.py
+++ amusementPark/amusementPark/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'amusementPark.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'amusementPark',
+        'NAME': 'amusementpark',
     }
 }
 
